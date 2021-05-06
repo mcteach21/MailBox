6905c986e67ebf756a22f320677f15a2026a04bc
MC <mc69.dashboard@gmail.com>
2021-05-06 10:02:51 +0200

    deprecated asynctask replace

--- app/src/main/java/mchou/apps/box/MainActivity.java
+++ app/src/main/java/mchou/apps/box/MainActivity.java
@@ -2,50 +2,70 @@ package mchou.apps.box;
 
 // https://stackoverflow.com/questions/42478637/how-to-read-emails-programmatically-in-android
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import javax.mail.*;
-import android.os.AsyncTask;
-
+import android.util.Log;
 import java.util.Properties;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "tests";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        new BackgroundTask(MainActivity.this) {
+            @Override
+            public void doInBackground() {
+                Properties props = new Properties();
+                props.setProperty("mail.store.protocol", "imaps");
+                try {
+                    Session session = Session.getInstance(props, null);
+                    Store store = session.getStore();
+                    store.connect("imap.gmail.com", "adm.mc69@gmail.com", "Azerty123_");
+                    Folder inbox = store.getFolder("INBOX");
+                    inbox.open(Folder.READ_ONLY);
 
-        new MyAsynk().execute();
+                    Log.i(TAG, "Inbox Messages : "+inbox.getMessageCount());
+
+                    Message msg = inbox.getMessage(inbox.getMessageCount());
+                    Address[] in = msg.getFrom();
+                    for (Address address : in) {
+                        Log.i(TAG , "doInBackground - FROM:" + address.toString());
+                    }
+                    Multipart mp = (Multipart) msg.getContent();
+                    BodyPart bp = mp.getBodyPart(0);
+                    Log.i(TAG ,"SENT DATE:" + msg.getSentDate());
+                    Log.i(TAG ,"SUBJECT:" + msg.getSubject());
+                    Log.i(TAG ,"CONTENT:" + bp.getContent());
+                } catch (Exception mex) {
+
+                    Log.e(TAG ,"Error : "+mex.getMessage());
+                    mex.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onPostExecute() {
+                Log.i(TAG, "onPostExecute: i've finished!");
+            }
+        }.start();
     }
 
-    public class MyAsynk extends AsyncTask<Void, Void, Void> {
-
-        @Override
-        protected Void doInBackground(Void... params) {
-            Properties props = new Properties();
-            props.setProperty("mail.store.protocol", "imaps");
-            try {
-                Session session = Session.getInstance(props, null);
-                Store store = session.getStore();
-                store.connect("imap.gmail.com", "youremail@gmail.com", "password");
-                Folder inbox = store.getFolder("INBOX");
-                inbox.open(Folder.READ_ONLY);
-                Message msg = inbox.getMessage(inbox.getMessageCount());
-                Address[] in = msg.getFrom();
-                for (javax.mail.Address address : in) {
-                    System.out.println("FROM:" + address.toString());
-                }
-                Multipart mp = (Multipart) msg.getContent();
-                BodyPart bp = mp.getBodyPart(0);
-                System.out.println("SENT DATE:" + msg.getSentDate());
-                System.out.println("SUBJECT:" + msg.getSubject());
-                System.out.println("CONTENT:" + bp.getContent());
-            } catch (Exception mex) {
-                mex.printStackTrace();
-            }
-            return null;
+    public abstract class BackgroundTask {
+        private AppCompatActivity activity;
+        public void start() {
+            new Thread(() -> {
+                doInBackground();
+                activity.runOnUiThread(() -> onPostExecute());
+            }).start();
         }
+        public BackgroundTask(AppCompatActivity activity) {
+            this.activity = activity;
+        }
+        public abstract void doInBackground();
+        public abstract void onPostExecute();
     }
 }
